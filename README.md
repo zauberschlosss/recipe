fc901726735f6ae71278905ed23152c9c755c3f3
Dmitriy Gordievskiy <aetmospearric.ein@gmail.com>
2020-10-01 21:23:27 +0300

    Initial setup

---
+++ README.md
@@ -0,0 +1,2 @@
+Recipe
+Persist to DB
---
+++ src/main/java/com/zauberschlosss/recipe/domain/Notes.java
@@ -0,0 +1,17 @@
+package com.zauberschlosss.recipe.domain;
+
+import javax.persistence.*;
+
+@Entity
+public class Notes {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    private Recipe recipe;
+
+    @Lob
+    private String recipeNotes;
+}
